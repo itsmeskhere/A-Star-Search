2bd1b60b1f3559eda00a6e9302b8b23249630cbf
itsmeskhere <sumankalyangiri786@gmail.com>
2023-11-06 01:21:26 +0530

    CMake update

---
+++ build/CMakeFiles/a_star_search.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/itsmesk/Code/C++/C++ Nanodegree/A-Star-Search/src/a_star_search.cpp" "CMakeFiles/a_star_search.dir/src/a_star_search.cpp.o" "gcc" "CMakeFiles/a_star_search.dir/src/a_star_search.cpp.o.d"
+  "/home/itsmesk/Code/C++/C++ Nanodegree/A-Star-Search/src/board.cpp" "CMakeFiles/a_star_search.dir/src/board.cpp.o" "gcc" "CMakeFiles/a_star_search.dir/src/board.cpp.o.d"
+  "/home/itsmesk/Code/C++/C++ Nanodegree/A-Star-Search/src/main.cpp" "CMakeFiles/a_star_search.dir/src/main.cpp.o" "gcc" "CMakeFiles/a_star_search.dir/src/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/a_star_search.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/a_star_search.dir/src/a_star_search.cpp.o"
+  "CMakeFiles/a_star_search.dir/src/a_star_search.cpp.o.d"
+  "CMakeFiles/a_star_search.dir/src/board.cpp.o"
+  "CMakeFiles/a_star_search.dir/src/board.cpp.o.d"
+  "CMakeFiles/a_star_search.dir/src/main.cpp.o"
+  "CMakeFiles/a_star_search.dir/src/main.cpp.o.d"
+  "a_star_search"
+  "a_star_search.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/a_star_search.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
